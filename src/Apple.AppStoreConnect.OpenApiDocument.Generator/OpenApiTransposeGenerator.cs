0209c9c3cc882460c3f3ce78dd20fd5db0e7f5cd
Jan Trejbal <jan@trejbal.land>
2023-04-18 14:59:41 +0200

    Bump JSON transpose structure

--- src/Apple.AppStoreConnect.OpenApiDocument.Generator/OpenApiTransposeGenerator.cs
+++ src/Apple.AppStoreConnect.OpenApiDocument.Generator/OpenApiTransposeGenerator.cs
@@ -3,7 +3,9 @@ using H.Generators.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 using System.Threading;
 
 namespace Apple.AppStoreConnect.OpenApiDocument.Generator;
@@ -31,36 +33,153 @@ public class OpenApiTransposeGenerator : IIncrementalGenerator
             )
             .SelectAndReportExceptions(GetSourceCode, context, Id);
 
-        context.RegisterSourceOutput(source, static (context, resultJson) =>
+        context.RegisterSourceOutput(source, static (context, properties) =>
         {
-            Directory.CreateDirectory(resultJson.Destination);
-
-            // This is a hack until source-generator support embedded resources
-            using var file = new FileStream(
-                Path.Combine(resultJson.Destination, resultJson.Filename),
-                FileMode.Create, FileAccess.Write, FileShare.Write
-            );
-
-            resultJson.Stream.CopyTo(file);
-            file.Flush(flushToDisk: true);
+            foreach (var property in properties)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(
+                    new DiagnosticDescriptor(
+                        $"{Id}_0001",
+                        "Processed property",
+                        $"Processed property {property}",
+                        "Apple.AppStoreConnect.OpenApiDocument.Generator",
+                        DiagnosticSeverity.Error,
+                        true
+                    ), null
+                ));
+            }
         });
     }
 
-    private static ResultJson GetSourceCode(
+    private static IEnumerable<string> GetSourceCode(
         (AdditionalText textFile, string resultOpenApiDestination) source,
         CancellationToken cancellationToken = default
     )
     {
-        return new ResultJson(
-            Destination: source.resultOpenApiDestination,
-            Filename: Path.GetFileName(source.textFile.Path),
-            Stream: File.OpenRead(source.textFile.Path)
+        var writerOptions = new JsonWriterOptions
+        {
+            Indented = true,
+        };
+
+        var documentOptions = new JsonDocumentOptions
+        {
+            CommentHandling = JsonCommentHandling.Skip,
+        };
+
+        Directory.CreateDirectory(source.resultOpenApiDestination);
+
+        // This is a hack until source-generator support embedded resources
+        using var destinationFile = new FileStream(
+            Path.Combine(source.resultOpenApiDestination, Path.GetFileName(source.textFile.Path)),
+            FileMode.Create, FileAccess.Write, FileShare.Write
         );
+
+        using var sourceJson = File.OpenRead(source.textFile.Path);
+        using var writer = new Utf8JsonWriter(destinationFile, options: writerOptions);
+
+        using var documentReader = JsonDocument.Parse(sourceJson, documentOptions);
+
+        var root = documentReader.RootElement;
+
+        if (root.ValueKind == JsonValueKind.Object)
+        {
+            writer.WriteStartObject();
+        }
+        else
+        {
+            yield break;
+        }
+
+
+        foreach (var property in root.EnumerateObject())
+        {
+            foreach (
+                var processComponent in ProcessSchema(
+                    property, writer,
+                    ProcessRoot
+                ))
+            {
+                yield return $"  {processComponent}";
+            }
+        }
+
+        writer.WriteEndObject();
+
+        writer.Flush();
+        destinationFile.Flush(flushToDisk: true);
     }
 
-    private record ResultJson(
-        string Destination,
-        string Filename,
-        Stream Stream
-    );
+    private static IEnumerable<string> ProcessRoot(JsonProperty component, Utf8JsonWriter writer)
+    {
+        yield return component.Name;
+
+        if (component.Name == "components")
+        {
+            foreach (
+                var processComponent in ProcessSchema(
+                    component, writer,
+                    ProcessComponents
+                ))
+            {
+                yield return $"  {processComponent}";
+            }
+        }
+        else
+        {
+            component.WriteTo(writer);
+        }
+    }
+
+    private static IEnumerable<string> ProcessComponents(JsonProperty component, Utf8JsonWriter writer)
+    {
+        yield return component.Name;
+
+        if (component.Name == "schemas")
+        {
+            foreach (
+                var processComponent in ProcessSchema(
+                    component, writer,
+                    ProcessComponentsSchema
+                ))
+            {
+                yield return $"  {processComponent}";
+            }
+        }
+        else
+        {
+            component.WriteTo(writer);
+        }
+    }
+
+    private static IEnumerable<string> ProcessComponentsSchema(JsonProperty component, Utf8JsonWriter writer)
+    {
+        yield return component.Name;
+        component.WriteTo(writer);
+    }
+
+    private static IEnumerable<string> ProcessSchema(
+        JsonProperty component,
+        Utf8JsonWriter writer,
+        Func<JsonProperty, Utf8JsonWriter, IEnumerable<string>> callback
+    )
+    {
+        if (component.Value.ValueKind == JsonValueKind.Object)
+        {
+            writer.WritePropertyName(component.Name);
+            writer.WriteStartObject();
+        }
+        else
+        {
+            yield break;
+        }
+
+        foreach (var property in component.Value.EnumerateObject())
+        {
+            yield return property.Name;
+
+            property.WriteTo(writer);
+        }
+
+        writer.WriteEndObject();
+    }
 }
