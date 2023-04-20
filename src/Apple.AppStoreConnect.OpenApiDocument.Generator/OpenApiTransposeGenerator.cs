9b8df9e5cc6c1c1d9a0a67762c0bb28033ff28d4
Jan Trejbal <jan@trejbal.land>
2023-04-20 14:13:43 +0200

    Implement JsonIterator

--- src/Apple.AppStoreConnect.OpenApiDocument.Generator/OpenApiTransposeGenerator.cs
+++ src/Apple.AppStoreConnect.OpenApiDocument.Generator/OpenApiTransposeGenerator.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Text.Json;
 using System.Threading;
 
@@ -60,10 +59,10 @@ public class OpenApiTransposeGenerator : IIncrementalGenerator
         CancellationToken cancellationToken = default
     )
     {
-        //if (!Debugger.IsAttached)
-        //{
-        //    Debugger.Launch();
-        //}
+        if (!Debugger.IsAttached)
+        {
+            Debugger.Launch();
+        }
 
         var writerOptions = new JsonWriterOptions
         {
@@ -96,7 +95,7 @@ public class OpenApiTransposeGenerator : IIncrementalGenerator
 
         try
         {
-            ReadJson(ref jsonReader, jsonWriter);
+            JsonIterator.ProcessJson(ref jsonReader, jsonWriter);
         }
         catch (Exception e)
         {
@@ -109,123 +108,5 @@ public class OpenApiTransposeGenerator : IIncrementalGenerator
         destinationFileStream.Flush(flushToDisk: true);
 
         yield break;
-    }
-
-    private static void ReadJson(
-        ref Utf8JsonReader jsonReader, Utf8JsonWriter jsonWriter
-    )
-    {
-        var path = new Stack<PathItem>();
-        ReadOnlySpan<byte> lastProperty = null;
-
-        while (jsonReader.Read())
-        {
-            var tokenType = jsonReader.TokenType;
-
-            switch (tokenType)
-            {
-                case JsonTokenType.StartObject:
-                    WritePathItem(tokenType, path, ref lastProperty);
-
-                    jsonWriter.WriteStartObject();
-                    break;
-                case JsonTokenType.EndObject:
-                    if (path.Count > 0)
-                    {
-                        path.Pop();
-                    }
-
-                    jsonWriter.WriteEndObject();
-                    break;
-                case JsonTokenType.StartArray:
-                    WritePathItem(tokenType, path, ref lastProperty);
-
-                    jsonWriter.WriteStartArray();
-                    break;
-                case JsonTokenType.EndArray:
-                    if (path.Count > 0)
-                    {
-                        path.Pop();
-                    }
-
-                    jsonWriter.WriteEndArray();
-                    break;
-                case JsonTokenType.PropertyName:
-                    if (jsonReader.HasValueSequence)
-                    {
-                        throw new Exception();
-                    }
-
-                    lastProperty = jsonReader.ValueSpan;
-
-                    jsonWriter.WritePropertyName(jsonReader.ValueSpan);
-
-                    ProcessItem(
-                        path, lastProperty,
-                        ref jsonReader, jsonWriter
-                    );
-                    break;
-
-                case JsonTokenType.None:
-                    break;
-                case JsonTokenType.Comment:
-                    break;
-
-                case JsonTokenType.String:
-                    if (jsonReader.HasValueSequence)
-                    {
-                        throw new Exception();
-                    }
-
-                    jsonWriter.WriteStringValue(jsonReader.ValueSpan);
-                    break;
-                case JsonTokenType.Number:
-                    jsonWriter.WriteNumberValue(jsonReader.GetInt32());
-                    break;
-                case JsonTokenType.True:
-                    jsonWriter.WriteBooleanValue(true);
-                    break;
-                case JsonTokenType.False:
-                    jsonWriter.WriteBooleanValue(false);
-                    break;
-                case JsonTokenType.Null:
-                    jsonWriter.WriteNullValue();
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-    }
-
-    private static void WritePathItem(
-        JsonTokenType tokenType,
-        Stack<PathItem> path, ref ReadOnlySpan<byte> lastProperty
-    )
-    {
-        if (!lastProperty.IsEmpty)
-        {
-            path.Push(new PathItem(tokenType, Encoding.UTF8.GetString(lastProperty.ToArray())));
-            lastProperty = null;
-        }
-        else
-        {
-            path.Push(new PathItem(tokenType, null));
-        }
-    }
-
-
-    private static void ProcessItem(
-        Stack<PathItem> path,
-        ReadOnlySpan<byte> lastProperty,
-        ref Utf8JsonReader jsonReader, Utf8JsonWriter jsonWriter
-    )
-    {
-        if (lastProperty.SequenceEqual("SubscriptionStatusUrlVersion"u8))
-        {
-            throw new Exception();
-        }
-        else if (lastProperty.SequenceEqual("enum"u8))
-        {
-        }
     }
 }
