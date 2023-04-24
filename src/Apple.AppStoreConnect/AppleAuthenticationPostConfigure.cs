11a895a80461a44cd1a48b1b3171c94e5d9b2215
Jan Trejbal <jan@trejbal.land>
2023-04-24 16:57:24 +0200

    Add default value for TokenAudience and JwtExpiresAfter

--- src/Apple.AppStoreConnect/AppleAuthenticationPostConfigure.cs
+++ src/Apple.AppStoreConnect/AppleAuthenticationPostConfigure.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.JsonWebTokens;
+using System;
 
 namespace Apple.AppStoreConnect;
 
@@ -8,12 +9,24 @@ public sealed class AppleAuthenticationPostConfigure : IPostConfigureOptions<App
     public void PostConfigure(string? name, AppleAuthenticationOptions options)
     {
         // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (options.TokenAudience is null)
+        {
+            options.TokenAudience = "appstoreconnect-v1";
+        }
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
         if (options.SecurityTokenHandler is null)
         {
             options.SecurityTokenHandler = new JsonWebTokenHandler
             {
                 SetDefaultTimesOnTokenCreation = false,
             };
+        }
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (options.JwtExpiresAfter == TimeSpan.Zero)
+        {
+            options.JwtExpiresAfter = TimeSpan.FromMinutes(20);
         }
     }
 }
