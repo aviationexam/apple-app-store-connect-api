e7482c1ffeff593130dd2c03a95658e2aeb9b71e
Jan Trejbal <jan@trejbal.land>
2023-04-29 13:41:00 +0200

    Add TwoGenerateJwtTokenWorks failing test

--- src/Test.Apple.AppStoreConnect/DefaultJwtGeneratorTest.cs
+++ src/Test.Apple.AppStoreConnect/DefaultJwtGeneratorTest.cs
@@ -62,4 +62,58 @@ public class DefaultJwtGeneratorTest
 
         Assert.NotNull(jwtToken);
     }
+
+    [Theory]
+    [InlineData(10)]
+    public async Task TwoGenerateJwtTokenWorks(int delay)
+    {
+        var serviceCollection = new ServiceCollection()
+            .Configure<AppleAuthenticationOptions>(x =>
+            {
+                x.KeyId = "TestKeyId";
+                x.IssuerId = "9E35F3F8-D597-45D3-84FE-5F8A386C070B";
+                x.TokenAudience = "appstoreconnect-v1";
+                x.PrivateKey = async (keyId, cancellationToken) =>
+                {
+                    var type = GetType();
+                    var fileProvider = new EmbeddedFileProvider(type.Assembly, type.Namespace);
+                    var fileInfo = fileProvider.GetFileInfo($"AuthKey_{keyId}.p8");
+                    await using var stream = fileInfo.CreateReadStream();
+                    using var reader = new StreamReader(stream);
+
+                    return (await reader.ReadToEndAsync(cancellationToken)).AsMemory();
+                };
+                x.JwtExpiresAfter = TimeSpan.FromSeconds(delay);
+            })
+            .AddSingleton<IJwtGenerator, DefaultJwtGenerator>()
+            .AddMemoryCache()
+            .AddLogging(loggingBuilder => loggingBuilder
+                .AddFilter("Apple.AppStoreConnect", LogLevel.Trace)
+                .AddConsole()
+                .AddDebug()
+            )
+            .AddSingleton<ISystemClock, SystemClock>()
+            .AddSingleton<CryptoProviderFactory>(_ => CryptoProviderFactory.Default);
+
+        serviceCollection.TryAddEnumerable(ServiceDescriptor
+            .Singleton<IPostConfigureOptions<AppleAuthenticationOptions>, AppleAuthenticationPostConfigure>()
+        );
+        serviceCollection.TryAddEnumerable(ServiceDescriptor
+            .Singleton<IValidateOptions<AppleAuthenticationOptions>, AppleAuthenticationOptionsValidate>()
+        );
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var jwtGenerator = serviceProvider.GetRequiredService<IJwtGenerator>();
+
+        var jwtToken = await jwtGenerator.GenerateJwtTokenAsync();
+
+        Assert.NotNull(jwtToken);
+
+        await Task.Delay(TimeSpan.FromSeconds(delay + 2));
+
+        var secondJwtToken = await jwtGenerator.GenerateJwtTokenAsync();
+
+        Assert.NotNull(secondJwtToken);
+    }
 }
