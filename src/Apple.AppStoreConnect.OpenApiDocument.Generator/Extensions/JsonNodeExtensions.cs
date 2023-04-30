6ed1d2973b34058860c062a97e269ba1c3e32f48
Jan Trejbal <jan@trejbal.land>
2023-05-01 00:39:51 +0200

    Add JsonNodeExtensions

---
+++ src/Apple.AppStoreConnect.OpenApiDocument.Generator/Extensions/JsonNodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Apple.AppStoreConnect.OpenApiDocument.Generator.Extensions;
+
+public static class JsonNodeExtensions
+{
+    public static JsonNode GetReferenceJsonNode(this string referenceName) => JsonNode.Parse($$"""
+    {
+      "$ref": "{{referenceName}}"
+    }
+    """)!;
+}
