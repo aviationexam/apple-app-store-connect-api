6aad485a769af97e46d2116b95e4aab310d04d1b
Jan Trejbal <jan@trejbal.land>
2023-05-16 13:08:11 +0200

    Add TooManyRequestsExtensions.TryParseLimits

---
+++ src/Apple.AppStoreConnect/Extensions/TooManyRequestsExtensions.cs
@@ -0,0 +1,70 @@
+using Apple.AppStoreConnect.Exceptions;
+using Apple.AppStoreConnect.Models;
+using System;
+using System.Linq;
+
+namespace Apple.AppStoreConnect.Extensions;
+
+public static class TooManyRequestsExtensions
+{
+    public const string UserHourLimit = "user-hour-lim";
+    public const string UserHourRemaining = "user-hour-rem";
+
+    public static ETooManyRequestParseLimit TryParseLimits(
+        this TooManyRequestsException exception,
+        out AppleRateLimitResponse rateLimitResponse
+    )
+    {
+        rateLimitResponse = new AppleRateLimitResponse();
+
+        if (!exception.Headers.TryGetValue(TooManyRequestsException.RateLimitHeader, out var rateLimitEnumerable))
+        {
+            return ETooManyRequestParseLimit.MissingHeader;
+        }
+
+        var rateLimits = rateLimitEnumerable.ToList();
+
+        if (rateLimits.Count > 1)
+        {
+            return ETooManyRequestParseLimit.TooManyHttpHeaders;
+        }
+
+        var rateLimit = rateLimits.Single();
+
+        var rateLimitProperties = rateLimit.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var rateLimitProperty in rateLimitProperties)
+        {
+            var keyValue = rateLimitProperty.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            if (keyValue.Length != 2)
+            {
+                return ETooManyRequestParseLimit.UnexpectedHeaderProperty;
+            }
+
+            if (int.TryParse(keyValue[1], out var intValue))
+            {
+                switch (keyValue[0])
+                {
+                    case UserHourLimit:
+                        rateLimitResponse = rateLimitResponse with { UserHourLimit = intValue };
+
+                        break;
+                    case UserHourRemaining:
+                        rateLimitResponse = rateLimitResponse with { UserHourRemaining = intValue };
+
+                        break;
+
+                    default:
+                        return ETooManyRequestParseLimit.HeaderContainsUnknownProperty;
+                }
+            }
+            else
+            {
+                return ETooManyRequestParseLimit.HeaderPropertyValueIsNotANumber;
+            }
+        }
+
+        return ETooManyRequestParseLimit.Success;
+    }
+}
