b3e840933c3d8ce0c13dbcbe2e7765cfcc382812
Jan Trejbal <jan@trejbal.land>
2024-08-17 18:09:45 +0200

    Add AppDeserializeTests

---
+++ src/Test.Apple.AppStoreConnect/AppDeserializeTests.cs
@@ -0,0 +1,49 @@
+﻿using Apple.AppStoreConnect;
+using Apple.AppStoreConnect.Converters;
+using Apple.AppStoreConnect.Interfaces;
+using H;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Time.Testing;
+using System;
+using System.Text.Json;
+using Xunit;
+
+namespace Test.Apple.AppStoreConnect;
+
+public class AppDeserializeTests
+{
+    [Fact]
+    public void DeserializeAppsResponseWorks()
+    {
+        var appsResponse = JsonSerializer.Deserialize<AppsResponse>(
+            Resources.app1_json.AsStream(),
+            CreateSerializerSettings()
+        );
+
+        Assert.NotNull(appsResponse);
+    }
+
+    private static JsonSerializerOptions CreateSerializerSettings()
+    {
+        var serviceCollection = new ServiceCollection()
+            .AddLogging()
+            .AddMemoryCache()
+            .AddSingleton<TimeProvider, FakeTimeProvider>()
+            .AddSingleton<IJwtGenerator, DefaultJwtGenerator>()
+            .AddSingleton<JsonStringEnumConverterFactory>()
+            .AddSingleton<OneOfJsonConverterFactory>()
+            .AddSingleton<IHttpClientConfiguration, DefaultHttpClientConfiguration>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var jsonSerializerOptions = new JsonSerializerOptions();
+
+        var httpClientConfiguration = serviceProvider.GetRequiredService<IHttpClientConfiguration>();
+        foreach (var jsonConverter in httpClientConfiguration.GetJsonConverters())
+        {
+            jsonSerializerOptions.Converters.Add(jsonConverter);
+        }
+
+        return jsonSerializerOptions;
+    }
+}
