dcb0a899082c05e2d5c36a3e101f7655f939160a
Jan Trejbal <jan@trejbal.land>
2024-08-17 16:59:47 +0200

    Fix typo

--- src/Apple.AppStoreConnect/OneOf.cs
+++ src/Apple.AppStoreConnect/OneOf.cs
@@ -2,7 +2,7 @@ namespace Apple.AppStoreConnect;
 
 #if DEBUG
 /// <summary>
-/// Placeholder to be able compile project without active H.NSwag.Generator
+/// Placeholder to be able to compile project without active H.NSwag.Generator
 /// </summary>
 public partial record OneOf;
 #endif
