d1dc99a2d6a774d85a320b56f3db77dda8864312
Jan Trejbal <jan@trejbal.land>
2024-08-27 20:55:28 +0200

    Configure Apple.AppStoreConnect.Playground

--- src/Apple.AppStoreConnect.Playground/Program.cs
+++ src/Apple.AppStoreConnect.Playground/Program.cs
@@ -10,7 +10,24 @@ var host = Host.CreateDefaultBuilder(args)
         .AddJsonFile("appsettings.json5", optional: false)
         .AddJsonFile("appsettings.Debug.json5", optional: true)
     )
-    .ConfigureServices(services => services.AddAppleAppStoreConnect())
+    .ConfigureServices(services => services
+        .AddSingleton(TimeProvider.System)
+        .AddMemoryCache()
+        .AddAppleAppStoreConnect(x => x
+            .Configure<IConfiguration>(static (x, configuration) =>
+                {
+                    var section = configuration.GetSection("AppStoreConnect");
+                    x.KeyId = section.GetValue<string>("KeyId")!;
+                    x.IssuerId = section.GetValue<string>("IssuerId")!;
+                    x.SetIssuedAt = section.GetValue<bool>("SetIssuedAt");
+                    x.JwtExpiresAfter = section.GetValue<TimeSpan>("JwtExpiresAfter");
+
+                    x.PrivateKey = (keyId, _) => Task.FromResult(section.GetSection("PrivateKey").GetValue<string>(keyId).AsMemory());
+                }
+            )
+            .ValidateOnStart()
+        )
+    )
     .Build();
 
 var appStoreConnectApiClient = host.Services.GetRequiredService<AppStoreConnectApiClient>();
