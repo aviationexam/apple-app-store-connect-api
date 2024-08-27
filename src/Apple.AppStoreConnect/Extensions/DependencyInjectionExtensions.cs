22297b97c1e5766b4a09437ab1ac1f053a6f77de
Jan Trejbal <jan@trejbal.land>
2024-08-27 19:00:48 +0200

    Add DependencyInjectionExtensions

---
+++ src/Apple.AppStoreConnect/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Apple.AppStoreConnect.Client;
+using Apple.AppStoreConnect.KiotaServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using System.Net.Http;
+
+namespace Apple.AppStoreConnect.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public const string AppStoreConnectHttpClient = "AppStoreConnect.Client";
+    public const string AppStoreConnectServiceKey = "AppStoreConnect";
+
+    public static IServiceCollection AddAppleAppStoreConnect(
+        this IServiceCollection serviceCollection
+    )
+    {
+        serviceCollection.AddHttpClient(AppStoreConnectHttpClient).AttachKiotaHandlers();
+
+        serviceCollection.AddKiotaHandlers();
+
+        serviceCollection.AddKeyedTransient<HttpClient>(
+                AppStoreConnectServiceKey,
+                (serviceProvider, _) => serviceProvider
+                    .GetRequiredService<IHttpClientFactory>()
+                    .CreateClient(AppStoreConnectHttpClient)
+            )
+            .AddKeyedTransient<IRequestAdapter, DefaultHttpClientRequestAdapter>(AppStoreConnectServiceKey)
+            .AddKeyedSingleton<IAuthenticationProvider, DefaultAuthenticationProvider>(AppStoreConnectServiceKey)
+            .AddTransient<AppStoreConnectApiClient>(serviceProvider => new AppStoreConnectApiClient(
+                serviceProvider.GetRequiredKeyedService<IRequestAdapter>(AppStoreConnectServiceKey)
+            ));
+
+        return serviceCollection;
+    }
+}
---
+++ src/Apple.AppStoreConnect/KiotaServices/DefaultAuthenticationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Apple.AppStoreConnect.KiotaServices;
+
+
+public sealed class DefaultAuthenticationProvider : IAuthenticationProvider
+{
+    public async Task AuthenticateRequestAsync(
+        RequestInformation request,
+        Dictionary<string, object>? additionalAuthenticationContext,
+        CancellationToken cancellationToken
+    )
+    {
+    }
+}
