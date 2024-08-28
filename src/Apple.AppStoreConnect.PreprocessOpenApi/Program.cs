39223280050526c39ad782c2087a5714c7149608
Jan Trejbal <jan@trejbal.land>
2024-08-28 13:12:27 +0200

    Add Apple.AppStoreConnect.PreprocessOpenApi

---
+++ src/Apple.AppStoreConnect.PreprocessOpenApi/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args is [{ } source, { } target])
+{
+    File.Copy(source, target);
+}
+else
+{
+    Console.WriteLine("Usage: <source> <target>");
+}
